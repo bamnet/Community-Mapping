6e4c5b6a3659207ec53c859940fd8326559ee9d4
Brian M <michab3@rpi.edu>
2011-02-08 18:54:15 -0500

    Validatign projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-
   validates :name, :presence => true, :uniqueness => true
-
+  validates :center_longitude, :numericality => true, :inclusion => { :in => -180..180 }
+  validates :center_latitude, :numericality => true, :inclusion => { :in => -90..90}
+  validates :default_zoom, :numericality =>{ :greater_than_or_equal_to => 0 }
 end
