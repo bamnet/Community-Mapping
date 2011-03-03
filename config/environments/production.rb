6f6078b8080dec73303ad75911dddf399e57e6da
Brian Michalski <bmichalski@gmail.com>
2011-03-03 11:01:25 -0500

    Trying to get mail working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ CommunityMapping::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -48,4 +48,10 @@ CommunityMapping::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'severe-dawn-182.heroku.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address  => "mail.rpi.edu",
+    :port  => 25
+  }
 end
